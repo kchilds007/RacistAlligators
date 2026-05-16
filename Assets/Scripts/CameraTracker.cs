b3a7ff3d9ddabfeb4daa843333b34e8180180741
gc <chavezagreyson@gmail.com>
2026-05-16 11:26:51 -0700

    added camera tracker

---
+++ Assets/Scripts/CameraTracker.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class CameraTracker : MonoBehaviour
+{
+    public Camera mainCamera ;
+
+    public Transform player;
+
+    private float screenHeight ;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mainCamera = Camera.main;
+        screenHeight = 2f * mainCamera.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        float topEdge = screenHeight / 2 + mainCamera.transform.position.y;
+        float bottomEdge = topEdge - screenHeight ;
+        
+        if (player.position.y > topEdge)
+        {
+            MoveCameraVertical(1);
+        } else if (player.position.y < bottomEdge)
+        {
+            MoveCameraVertical(-1);
+        }
+        
+        
+    }
+
+    public void MoveCameraVertical(int multiplier)
+    {
+        mainCamera.transform.position = new Vector3(mainCamera.transform.position.x,
+            mainCamera.transform.position.y + screenHeight * multiplier, mainCamera.transform.position.z);
+    }
+    
+}
