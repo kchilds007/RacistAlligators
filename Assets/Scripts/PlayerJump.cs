1bce896850c2c008a9988a15aaaf49f9dddbc837
Tyler Schweizer <tschweizer@chapman.edu>
2026-05-19 11:38:42 -0700

    Fixed the jump bug

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -58,6 +58,7 @@ public class PlayerJump : MonoBehaviour
             {
                 jumpPower = GameParameters.maxJumpPower;
             }
+            print("JumpPoer = " + jumpPower);
         }
         else
         {
@@ -79,14 +80,19 @@ public class PlayerJump : MonoBehaviour
                 spriteRenderer.sprite = sprites[5];
                 animator.enabled = true;
             }
+
+            jumpPower = jumpPower * 2;
+            
             //rigidbody2D.AddForceY(, ForceMode2D.Impulse);
             if (!player.facingLeft())
             {
-                rigidbody2D.AddForce(new Vector2((jumpPower / 3) * -1, jumpPower), ForceMode2D.Impulse);
+                rigidbody2D.AddForce(new Vector2((jumpPower/3) * -1.0f, jumpPower), ForceMode2D.Impulse);
+                print("Jumping: " + (jumpPower/3) * -1 + ", " + jumpPower);
             }
             else
             {
-                rigidbody2D.AddForce(new Vector2((jumpPower / 3), jumpPower), ForceMode2D.Impulse);
+                rigidbody2D.AddForce(new Vector2((float)(jumpPower/3), jumpPower), ForceMode2D.Impulse);
+                print("Jumping: " + (jumpPower/3)  + ", " + jumpPower);
             }
             
             jumpPower = GameParameters.minJumpPower;
@@ -99,6 +105,7 @@ public class PlayerJump : MonoBehaviour
         
         if (other.gameObject.CompareTag("StickyGround"))
         {
+            print("On sticky ground");
             isOnStickyGround = true;
             GameParameters.maxJumpPower = originalMaxJumpPower / 2f;
         }
