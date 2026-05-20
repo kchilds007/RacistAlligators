66907f647f64e768f1ffdfacca47e0f90b088097
gc <chavezagreyson@gmail.com>
2026-05-20 16:31:20 -0700

    added background music playlist

---
+++ Assets/Scripts/BackgroundMusicPlayer.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BackgroundMusicPlayer : MonoBehaviour
+{
+    // need list of audio clips/songs to play
+    public AudioClip[] songs;
+    
+    // need an audio source
+    private AudioSource audioSource;
+    private int currentSongIndex = 0;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = songs[currentSongIndex];
+            audioSource.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if a song isn't playing, choose a random song and play
+        if (!audioSource.isPlaying)
+        {
+            currentSongIndex = Random.Range(0, songs.Length);
+            audioSource.clip = songs[currentSongIndex];
+            audioSource.Play();
+        }
+    }
+}
