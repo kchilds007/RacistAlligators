890d44bda467d62e22245cde5b557ce5f3f204e0
Tyler Schweizer <tschweizer@chapman.edu>
2026-05-21 10:34:04 -0700

    change jump charge rate so its not so slow

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -8,7 +8,7 @@ public static class GameParameters
     public static float velPower = 2f;
     public static float maxJumpPower = 20f;
     public static float minJumpPower = 0.5f;
-    public static float variableJumpPower = 0.1f;
+    public static float variableJumpPower = 0.2f;
     public static bool isJumping = false;
 
     public static float PatrollingEnemyspeed = 5;
